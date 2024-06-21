0f9cc5cc752559ed6bf16cd7f14b447a7443ac68
anggunpribadi <anxggxun@gmail.com>
2024-06-22 04:51:57 +0700

    change posts compact route

--- pages/api/posts-compact.tsx
+++ pages/api/posts-compact.tsx
@@ -46,6 +46,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 slug: post.slug,
                 tags: post.tags,
                 categories: post.categories,
+                videoID: post.videoID
             })
         ) 
     })
